dac9146b1e6795da214a3460ba2a5ce43e1e3907
Gregori Farias <gregori.f16@gmail.com>
2024-03-17 12:44:30 -0400

    Initial Commit

---
+++ Tests/MyKeyChainPackageTests/MyKeyChainPackageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import MyKeyChainPackage
+
+final class MyKeyChainPackageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
